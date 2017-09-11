16b038bc4d844d20faf66f783ebb1a88c937638c
Ricky Chan <rickysychan@gmail.com>
2017-09-11 21:23:25 +0000

    implemented function getRepoContributors)

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 const request = require('request');
+// const fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -6,14 +7,31 @@ var GITHUB_USER = "rizelmine17";
 var GITHUB_TOKEN = "4b4f3706087ba2d1f907b57dba3662257548f948";
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
                    '@api.github.com/repos/' + repoOwner + '/' +
                     repoName + '/contributors';
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': GITHUB_USER,
+      'Authorization': 'token ' + GITHUB_TOKEN
+    }
+  }
 
-console.log(requestURL)
+  request.get(options, function(error, response, body) {
+   if (response && response.statusCode == 200) {
+      var parsed = JSON.parse(body);
+      cb(parsed);
+    }
+  })
+
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+function displayBody(parsedInput){
+  parsedInput.forEach(function (item) {
+    console.log(item.avatar_url)
+  })
+}
+
+getRepoContributors("jquery", "jquery", displayBody)
