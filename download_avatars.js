0d85e31714adddb08e4ed2d7d24270a84fc63c1f
Ricky Chan <rickysychan@gmail.com>
2017-09-11 20:06:37 +0000

    created requestURL variable and tested

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,16 @@
 const request = require('request');
 
-const url =  "https://api.github.com/users/lighthouse-labs"
-
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "rizelmine17";
+var GITHUB_TOKEN = "4b4f3706087ba2d1f907b57dba3662257548f948";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
+                   '@api.github.com/repos/' + repoOwner + '/' +
+                    repoName + '/contributors';
+
+console.log(requestURL)
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
