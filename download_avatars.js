9ac3dd67aa4a5893de7df58a17ae5af890f40573
Ricky Chan <rickysychan@gmail.com>
2017-09-11 19:59:51 +0000

    started getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
-var request = require('request');
+const request = require('request');
+
+const url =  "https://api.github.com/users/lighthouse-labs"
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
